02be3bde4afa41fbcaa8ff0f7baba2624f1ac7c7
Luis F <25207740+0x4c4652@users.noreply.github.com>
2019-03-25 17:02:28 -0400

    Added file extension, updated formatting.

--- cassidoo-squareCubeNum.py
+++ cassidoo-squareCubeNum.py
@@ -5,6 +5,7 @@ def squareCubeNum(num: int):
   posNum = abs(num) # takes the absolute value of the input, making it positive.
   
   highestCubeRoot = round(pow(posNum, 1/3)) # lets the application quickly iterate through the closest cubed numbers to the input
+  
   for i in range(1, highestCubeRoot, 1):
     perfectCube = pow(i, 3) # we know this is a perfect cube
     if ((pow(perfectCube, 1/2) == round(pow(perfectCube, 1/2)))): # check if the perfect cube has a perfect square
